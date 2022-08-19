a83f69589eb5fb62482bb3214aca2d4a7dd2c544
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-08-19 13:42:53 +0100

    Updated

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      'https://ingredients-a74d5-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((resData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: resData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
+
+  return (
+    <div className='App'>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
